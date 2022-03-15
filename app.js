4dc566996060025dc7b4d8e0d30a9a2eb9d484ed
kadirkara22 <kadirkar2204@gmail.com>
2022-03-15 23:31:38 +0300

    görsel ekleme işlemi yapıldı

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path')
+const fs = require('fs')
+const fileUpload = require('express-fileupload');
 const Photo = require('./modals/Photo');
 const mongoose = require('mongoose');
+const { fstat } = require('fs');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost/pcat-test-db')
 
+/* mongoose.connect('mongodb+srv://kadir:123456789.@cluster0.qpmrg.mongodb.net/pcat-db?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('DB CONNECTED!')
+    }).catch((err) => {
+        console.log(err)
+    }) */
+
 app.set("view engine", "ejs");
 
 app.use(express.static('public'))
-
+app.use(fileUpload());
 
 
 app.get('/', async (req, res) => {
-    const photos = await Photo.find({})
+    const photos = await Photo.find({}).sort('-dateCreated')
     res.render('index', {
         photos
     })
 })
+
+app.get('/photos/:id', async (req, res) => {
+    const photo = await Photo.findById(req.params.id)
+    res.render('photo', {
+        photo
+    })
+})
+
 app.get('/about', (req, res) => {
     res.render('about')
 })
@@ -33,8 +51,26 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.post('/photos', async (req, res) => {
-    await Photo.create(req.body)
-    res.redirect('/')
+    //console.log(req.files.image)
+    //await Photo.create(req.body)
+    //res.redirect('/')
+
+    const uploadDir = 'public/uploads'
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir)
+    }
+
+    let uploadeImage = req.files.image
+    let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name
+
+    uploadeImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadeImage.name
+        })
+        res.redirect('/')
+    })
+
 })
 const port = 3000;
 
